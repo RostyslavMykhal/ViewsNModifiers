871bc2cdc851f291332a8eb710397ff151656963
Rostyslav Mykhal <millersdev.ios@gmail.com>
2023-11-28 15:49:12 +0200

    Complete project
    Complete project

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Elliott Miller on 28.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
